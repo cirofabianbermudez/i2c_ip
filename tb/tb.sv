ede53985baf3b8e9ff1044c067542c4cc0eaaa2e
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-06-21 18:46:05 +0200

    feat: add tb.sv and test.sv
    - tb.sv is the entry point of the testbench
    - test.sv contains task for START, STOP and WRITE commands

---
+++ tb/tb.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 100ps
+
+module tb;
+
+  // Clock signal
+  parameter CLK_PERIOD = 10ns;
+  logic clk = 0;
+  always #(CLK_PERIOD / 2) clk = ~clk;
+
+  // Interface
+  i2c_if vif(clk);
+
+  // Test
+  test top_test(vif);
+
+  // Instantiation
+  i2c_master dut (
+    .clk(vif.clk),
+    .rst(vif.rst),
+    .din(vif.din),
+    .dvsr(vif.dvsr),
+    .cmd(vif.cmd),
+    .wr_i2c(vif.wr_i2c),
+    .scl(vif.scl),
+    .sda(vif.sda),
+    .ready(vif.ready),
+    .done_tick(vif.done_tick),
+    .ack(vif.ack),
+    .dout(vif.dout)
+  );
+
+  initial begin
+    $timeformat(-9, 0, "ns", 10);
+  end
+
+endmodule : tb
---
+++ tb/test.sv
@@ -0,0 +1,63 @@
+module test (i2c_if.dvr vif);
+
+  localparam Divisor = 10'd20;
+  localparam START_CMD   = 3'b000;
+  localparam WR_CMD      = 3'b001;
+  localparam RD_CMD      = 3'b010;
+  localparam STOP_CMD    = 3'b011;
+  localparam RESTART_CMD = 3'b100;
+   
+  initial begin
+    $display("Begin Of Simulation.");
+    reset();
+    start();
+    write(8'd11);
+    write(8'd12);
+    stop();
+    $display("End Of Simulation.");
+    $finish;
+  end
+
+  task reset();
+    vif.rst  = 1'b1;            // Assert async reset
+    vif.dvsr = Divisor;         // 20 clock cycles
+    vif.cb.din    <= 8'd10;     // Default din value
+    vif.cb.cmd    <= START_CMD;  
+    vif.cb.wr_i2c <= 1'b0;      // Default wr
+    repeat(2) @(vif.cb);
+    vif.cb.rst    <= 1'b0;      // Deassert reset
+    repeat ((Divisor+1)*4) @(vif.cb);
+  endtask : reset
+
+  task start();
+    vif.cb.cmd    <= START_CMD;   // START_CMD
+    vif.cb.wr_i2c <= 1'b1;        // Start write
+    @(vif.cb);
+    vif.cb.wr_i2c <= 1'b0;       // Deassert
+    wait(vif.cb.ready != 0);
+    @(vif.cb iff( vif.cb.ready == 1 ) );
+    @(vif.cb);
+  endtask : start
+
+  task write(input logic [7:0] value);
+    vif.cb.din    <= value;    // Default din value
+    vif.cb.cmd    <= WR_CMD;   // WR_CMD
+    vif.cb.wr_i2c <= 1'b1;     // Start write
+    @(vif.cb);
+    vif.cb.wr_i2c <= 1'b0;     // Deassert
+    wait(vif.cb.ready != 0);
+    @(vif.cb iff( vif.cb.ready == 1 ) );
+    @(vif.cb);
+  endtask : write
+  
+  task stop();
+    vif.cb.cmd    <= STOP_CMD;   // WR_CMD
+    vif.cb.wr_i2c <= 1'b1;     // Start write
+    @(vif.cb);
+    vif.cb.wr_i2c <= 1'b0;     // Deassert
+    wait(vif.cb.ready != 0);
+    @(vif.cb iff( vif.cb.ready == 1 ) );
+    @(vif.cb);
+  endtask : stop
+
+endmodule : test
