92a80fd9c82a9156e30c4d0e1da94303b72ae1cc
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-06-25 19:06:35 +0200

    feat: add detection for START and STOP condition

--- tb/test.sv
+++ tb/test.sv
@@ -9,13 +9,25 @@ module test (i2c_if.dvr vif);
    
   initial begin
     $display("Begin Of Simulation.");
-    reset();
-    start();
-    write(8'd11);
-    write(8'd12);
-    stop();
-    start();
-    restart();
+    fork
+      begin
+        reset();
+        start();
+        write(8'hfa);
+        write(8'd12);
+        stop();
+        start();
+        write(8'hfa);
+        restart();
+        write(8'd21);
+        write(8'd22);
+        stop();
+      end
+      begin
+        check_start();
+      end
+    join
+   
     $display("End Of Simulation.");
     $finish;
   end
@@ -71,5 +83,56 @@ module test (i2c_if.dvr vif);
     @(vif.cb iff( vif.cb.ready == 1 ) );
     @(vif.cb);
   endtask : restart
+  
+  task check_start();
+    forever begin
+
+        // negedge SDA
+        wait(vif.cb.sda != 0);
+        @(vif.cb iff( vif.cb.sda == 0 ) );
+        $display("[INFO] %4t: negedge SDA", $realtime);
+
+        if (vif.cb.scl == 0) begin
+          continue;
+        end
+
+        // negedge SCL
+        wait(vif.cb.scl != 0);
+        @(vif.cb iff( vif.cb.scl == 0) );
+        $display("[INFO] %4t: negedge SCL", $realtime);
+        
+        if (vif.cb.scl == 1'b0 &&  vif.cb.scl == 1'b0) begin
+          $display("[INFO] %4t: START CONDITION detected", $realtime);
+        end else begin
+          continue;
+        end
+
+        forever begin
+          // posedge SCL
+          wait(vif.cb.scl != 1);
+          @(vif.cb iff( vif.cb.scl == 1) );
+          $display("[INFO] %4t: posedge SCL", $realtime);
+
+          if (vif.cb.sda == 1) begin
+            continue;
+          end
+
+          // posedge SDA
+          wait(vif.cb.sda != 1);
+          @(vif.cb iff( vif.cb.sda == 1 ) );
+          $display("[INFO] %4t: posedge SDA", $realtime);
+
+          if (vif.cb.scl == 1'b1 &&  vif.cb.scl == 1'b1) begin
+            $display("[INFO] %4t: STOP CONDITION detected", $realtime);
+            break;
+          end else begin
+            continue;
+          end
+
+        end
+
+        
+    end
+  endtask : check_start
   
 endmodule : test
